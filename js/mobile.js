f3ab3b6576b007c481fd2afd0f9b74c9d7dcf107
Sheva Alex <shevaalexey@gmail.com>
2021-11-15 20:07:30 +0200

    Fix styles

--- js/mobile.js
+++ js/mobile.js
@@ -1,27 +1,35 @@
 $(function() {
     function slideMenu() {
-      var activeState = $("#menu-container .menu-list").hasClass("active");
-      $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
+        var activeState = $("#menu-container .menu-list").hasClass("active");
+        $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
     }
     $("#menu-wrapper").click(function(event) {
-      event.stopPropagation();
-      $("#hamburger-menu").toggleClass("open");
-      $("#menu-container .menu-list").toggleClass("active");
-      slideMenu();
-  
-      $("body").toggleClass("overflow-hidden");
+        event.stopPropagation();
+        $("#hamburger-menu").toggleClass("open");
+        $("#menu-container .menu-list").toggleClass("active");
+        slideMenu();
+
+        $("body").toggleClass("overflow-hidden");
     });
   
     $(".menu-list").find(".accordion-toggle").click(function() {
-      $(this).next().toggleClass("open").slideToggle("fast");
-      $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
+        $(this).find('.submenu').toggleClass("open").slideToggle("fast");
+        $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
   
-      $(".menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
-      $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
+        $(".menu-list .accordion-content").not($(this).find('.submenu')).slideUp("fast").removeClass("open");
+        $(".menu-list .accordion-toggle").not($(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
     });
 
-    $("#logo").before($("nav"));
-  }); 
+    $(".menu-link").click(function(){
+        $(this).parent().trigger('click');
+
+        return false;
+    });
+
+    if (window.screen.width <= 760) {
+        $("#logo").before($("nav"));
+    }
+});
 
   
   
